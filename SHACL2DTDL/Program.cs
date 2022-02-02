346ba9f433d25b4eec8c4969832b52f7edd7d604
Karl Hammar <karl.hammar@outlook.com>
2022-02-02 21:26:19 +0100

    Stub for SHACL2DTDL converter

---
+++ SHACL2DTDL/Program.cs
@@ -0,0 +1,15 @@
+﻿using VDS.RDF;
+
+IGraph g = new Graph();
+g.BaseUri = new Uri("http://example.org/");
+
+var karl = g.CreateUriNode(new Uri("https://karlhammar.com/me"));
+var name = g.CreateUriNode(new Uri("https://karlhammar.com/name"));
+var karlsname = g.CreateLiteralNode("Karl Hammar");
+
+g.Assert(new Triple(karl, name, karlsname));
+
+foreach (Triple t in g.Triples)
+{
+    Console.WriteLine(t.ToString());
+}
